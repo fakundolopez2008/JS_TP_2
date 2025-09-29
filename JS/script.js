1d119f3bd64194a383b0f54da053ff79df484e05
Facundo Lopez Ordoqui <fakulodornequi@hotmail.com.ar>
2025-09-29 00:15:07 -0300

    Primer Commit

---
+++ JS/script.js
@@ -0,0 +1,25 @@
+function AgroQuimico (nombre, medida, precio){
+    this.nombre = nombre;
+    this.medida = medida;
+    this.precio = precio;
+}
+
+const productos = [
+new AgroQuimico ("Glifosato", "1 litro", 1500),
+new AgroQuimico ("Atrazina", "1 litro", 2000),
+new AgroQuimico ("Clorpirifos", "1 litro", 2500),
+new AgroQuimico ("Paraquat", "1 litro", 1800),
+new AgroQuimico ("2,4-D", "1 litro", 2200),
+new AgroQuimico ("Metamidofós", "1 litro", 3000),
+];
+
+let carrito = [];
+
+let botones = document.querySelectorAll("button");
+botones.forEach((boton,index) => {
+  boton.addEventListener("click", ( ) => {
+    carrito.push(productos[index]);
+    console.log(carrito);
+  });
+});
+
