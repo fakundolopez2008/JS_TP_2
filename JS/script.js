4722bf6e74f59b843630eed4c3bdb3a193849d98
Facundo Lopez <fakulodornequi@hotmail.com.ar>
2025-10-04 21:13:28 -0300

    Correccion mostrarCarrito. Corregir el appendChild de este

--- JS/script.js
+++ JS/script.js
@@ -4,29 +4,27 @@ function AgroQuimico (nombre, medida, precio){
     this.precio = precio;
 }
 function mostrarCarrito(carritoItems,contadorItems){
-  if(carritoItems.length > 0){
-    contadorItems.addEventListener("click", () => {
-    const listaCarrito = document.createElement("article");
-    listaCarrito.innerHTML= `<img src="./images/glifosato.png" alt="Agroquímico 1">
+    const listaCarrito = document.createElement("section");
+    listaCarrito.classList.add("carrito-flotante");
+    carritoItems.map((item) => {
+    listaCarrito.innerHTML= `<article class="producto">
+        <img src="./images/glifosato.png" alt="Agroquímico 1">
         <h3>Glifosato</h3>
         <p>Medida: 1 litro</p>
-        <p>Precio: $1500</p>`
-        listadoCarrito.appendChild(listaCarrito);
+        <p>Precio: $1500</p>
+      </article>`
     });
-  } else if(carritoItems.length === 0){
-    contadorItems.addEventListener("click", () => {
-      alert("El carrito está vacío");
-    });
-  }
+      contadorItems.appendChild(listaCarrito);
+    
 }
 
 const productos = [
-new AgroQuimico ("Glifosato", "1 litro", 1500),
-new AgroQuimico ("Atrazina", "1 litro", 2000),
-new AgroQuimico ("Clorpirifos", "1 litro", 2500),
-new AgroQuimico ("Paraquat", "1 litro", 1800),
-new AgroQuimico ("2,4-D", "1 litro", 2200),
-new AgroQuimico ("Metamidofós", "1 litro", 3000),
+  new AgroQuimico("Glifosato", "1 litro", 1500, "./images/glifosato.png"),
+  new AgroQuimico("Atrazina", "1 litro", 2000, "./images/atrazina-01.jpg"),
+  new AgroQuimico("Clorpirifos", "1 litro", 2500, "./images/22222-35601.jpg"),
+  new AgroQuimico("Paraquat", "1 litro", 1800, "./images/PARAQUAT-VECOL.png"),
+  new AgroQuimico("2,4-D", "1 litro", 2200, "./images/2_4_d_action.jpg"),
+  new AgroQuimico("Metamidofós", "1 litro", 3000, "./images/Modelo-3D-Metamidofos.jpg"),
 ];
 
 let carrito = [];
@@ -37,8 +35,14 @@ botones.forEach((boton,index) => {
   boton.addEventListener("click", () => {
     carrito.push(productos[index]);
     contador.innerText = `Carrito ${carrito.length}`;
-    console.log(carrito);
   });
 });
 
-mostrarCarrito(carrito,contador);
+contador.addEventListener("click", () => {
+if(carrito.length > 0){
+      mostrarCarrito(carrito,contador);
+    } else{
+        alert("El carrito está vacío");
+      }
+  }
+);
