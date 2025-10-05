6dd8e73c12132e12c49f39015920eac788a3cfea
Facundo Lopez Ordoqui <fakulodornequi@hotmail.com.ar>
2025-10-05 11:18:27 -0300

    Adicion JQuery - Productos.JSON

--- JS/script.js
+++ JS/script.js
@@ -1,48 +1,59 @@
-function AgroQuimico (nombre, medida, precio){
+function AgroQuimico (nombre, medida, precio, imagen){
     this.nombre = nombre;
     this.medida = medida;
     this.precio = precio;
+    this.imagen = imagen;
 }
+
 function mostrarCarrito(carritoItems,contadorItems){
-    const listaCarrito = document.createElement("section");
-    listaCarrito.classList.add("carrito-flotante");
-    carritoItems.map((item) => {
-    listaCarrito.innerHTML= `<article class="producto">
-        <img src="./images/glifosato.png" alt="Agroquímico 1">
-        <h3>Glifosato</h3>
-        <p>Medida: 1 litro</p>
-        <p>Precio: $1500</p>
-      </article>`
-    });
+  carritoItems.forEach((productos) => {
+    const listaCarrito = document.createElement("article");
+    listaCarrito.classList.add("carritoFlotante");
+      listaCarrito.innerHTML= `<img src="${productos.imagen}" alt="${productos.nombre}">
+        <h3>${productos.nombre}</h3>
+        <p>Medida: ${productos.medida}</p>
+        <p>Precio: $${productos.precio}</p>`;
       contadorItems.appendChild(listaCarrito);
-    
+    });
+  }  
+      
+
+function mostrarCarritoVacio(contadorItems){
+    const listaCarrito = document.createElement("article");
+    listaCarrito.classList.add("carritoFlotante");
+    listaCarrito.innerHTML= `<article>
+      <p>El carrito está vacío</p>
+      </article>`;
+      contadorItems.appendChild(listaCarrito);
+      contador.addEventListener("mouseout", () => {
+      listaCarrito.remove();
+  })
 }
 
-const productos = [
-  new AgroQuimico("Glifosato", "1 litro", 1500, "./images/glifosato.png"),
-  new AgroQuimico("Atrazina", "1 litro", 2000, "./images/atrazina-01.jpg"),
-  new AgroQuimico("Clorpirifos", "1 litro", 2500, "./images/22222-35601.jpg"),
-  new AgroQuimico("Paraquat", "1 litro", 1800, "./images/PARAQUAT-VECOL.png"),
-  new AgroQuimico("2,4-D", "1 litro", 2200, "./images/2_4_d_action.jpg"),
-  new AgroQuimico("Metamidofós", "1 litro", 3000, "./images/Modelo-3D-Metamidofos.jpg"),
-];
+$.getJSON("./data/productos.json")
+  .done(function(data) {
+    console.log("Datos cargados:", data);
+    let carrito = [];
+    const botones = document.querySelectorAll("#agregar");
+    const contador = document.querySelector("#contador");
 
-let carrito = [];
-const botones = document.querySelectorAll("#agregar");
-const contador = document.querySelector("#contador");
-
-botones.forEach((boton,index) => {
-  boton.addEventListener("click", () => {
-    carrito.push(productos[index]);
-    contador.innerText = `Carrito ${carrito.length}`;
-  });
+    botones.forEach((boton,index) => {
+      boton.addEventListener("click", () => {
+        carrito.push(data[index]);
+        contador.innerText = `Carrito (${carrito.length})`;
+      });
 });
 
-contador.addEventListener("click", () => {
-if(carrito.length > 0){
-      mostrarCarrito(carrito,contador);
-    } else{
-        alert("El carrito está vacío");
+contador.addEventListener("mouseover", () => {
+  if(carrito.length === 0){
+      mostrarCarritoVacio(contador);
+    } else{        
+        mostrarCarrito(carrito,contador);
       }
-  }
+  }  
 );
+  })
+  .fail(function(error) {
+    console.error("Error al cargar los datos:", error);
+  });
+
